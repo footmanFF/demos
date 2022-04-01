5d3ed4eb3829e21cb07a2c84c46a8a1c4c021fd5
footmanff <li.zhang04@weimob.com>
2022-04-01 20:09:40 +0800

    cglib

--- origin/src/main/java/com/fm/framework/joinery/JoineryTest.java
+++ origin/src/main/java/com/fm/framework/joinery/JoineryTest.java
@@ -1,7 +1,7 @@
 package com.fm.framework.joinery;
 
-import joinery.DataFrame;
-import joinery.impl.Combining;
+//import joinery.DataFrame;
+//import joinery.impl.Combining;
 import org.junit.Test;
 import java.util.Arrays;
 
@@ -10,21 +10,21 @@ import java.util.Arrays;
  */
 public class JoineryTest {
 
-    @Test
-    public void joinTest() throws Exception {
-        DataFrame left = new DataFrame<>();
-        left.add("key", "b1", "c1");
-        left.append(Arrays.asList(1, 2, 3));
-        left.append(Arrays.asList(2, 2, 3));
-        left = left.reindex("key", false);
-
-        DataFrame right = new DataFrame<>();
-        right.add("key", "b2", "c2");
-        right.append(Arrays.asList(3, 2, 3));
-        right = right.reindex("key", false);
-
-        DataFrame result = Combining.join(left, right, joinery.DataFrame.JoinType.LEFT, null);
-        System.out.println(result);
-    }
+//    @Test
+//    public void joinTest() throws Exception {
+//        DataFrame left = new DataFrame<>();
+//        left.add("key", "b1", "c1");
+//        left.append(Arrays.asList(1, 2, 3));
+//        left.append(Arrays.asList(2, 2, 3));
+//        left = left.reindex("key", false);
+//
+//        DataFrame right = new DataFrame<>();
+//        right.add("key", "b2", "c2");
+//        right.append(Arrays.asList(3, 2, 3));
+//        right = right.reindex("key", false);
+//
+//        DataFrame result = Combining.join(left, right, joinery.DataFrame.JoinType.LEFT, null);
+//        System.out.println(result);
+//    }
 
 }
