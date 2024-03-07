3d649ecf76a28640dcba66362942b8b5e4b40ddc
footmanff <li.zhang04@weimob.com>
2024-03-07 22:47:48 +0800

    各种用例

--- jdk/src/main/java/com/footmanff/jdktest/concurrent/CountDownLatchTest.java
+++ jdk/src/main/java/com/footmanff/jdktest/concurrent/CountDownLatchTest.java
@@ -2,11 +2,23 @@ package com.footmanff.jdktest.concurrent;
 
 import org.junit.Test;
 
+import java.util.concurrent.CountDownLatch;
+
 public class CountDownLatchTest {
-    
+
     @Test
-    public void t1() {
-                
+    public void t1() throws Exception {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        new Thread(() -> {
+            try {
+                Thread.sleep(1000L);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            countDownLatch.countDown();
+        }).start();
+        countDownLatch.await();
+        System.out.println("end");
     }
-    
+
 }
